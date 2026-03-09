a11c845db41f8b36f01ac060d37f5b28cdc5da67
naomyamador_04 <naomyabigail2009@gmail.com>
2026-03-09 00:13:16 -0400

    Tasas de cambio en 3 paises diferentes

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ tasa_de_cambio = float(input("Ahora, ingrese la tasa de cambio del país: "))
 
 resultado = exchange_money(dinero_original, tasa_de_cambio)
 print("El equivalente de la moneda extrajera elegida es: ", resultado)
+
+#Tasa de Cambio de monedas extrangeras para probar el programa
+#Colombia: 1 USD = 0.000265
+#Mexico: 1 USD = 0.059
+#Reino Unido: 1 USD = 0.787
