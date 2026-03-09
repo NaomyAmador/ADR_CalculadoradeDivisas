729cf5cecf67e3e93b542fde312f3346c4ca00c2
naomyamador_04 <naomyabigail2009@gmail.com>
2026-03-08 23:58:20 -0400

    Interacción con el usuario para la obtención de datos hecha

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 def exchange_money(dinero_original, tasa_de_cambio):
     moneda_extrangera = dinero_original / tasa_de_cambio
     return moneda_extrangera
+
+#Interacción con el usuario para conseguir datos
+input("¡Bienvenido/a Viajero a la Calculadora de Divisas!")
+
+dinero_original = float(input("Ingrese la cantidad de dinero que desea cambiar: "))
+tasa_de_cambio = float(input("Ahora, ingrese la tasa de cambio del país: "))
+
+resultado = exchange_money(dinero_original, tasa_de_cambio)
+print("El equivalente de la moneda extrajera elegida es: ", resultado)
